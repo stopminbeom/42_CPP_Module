2ff210b8c96da96397e7181ccdad83e9721a494a
stopminbeom <mseo@student.42seoul.kr>
2022-01-25 15:12:33 +0900

    Feat: cpp07 end

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 30
+#define MAX_VAL 10
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
+    std::cout << "Number size : " << numbers.size() << std::endl;
+
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
+    std::cout << "set value in number & mirror" << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
-
+    Array<int> tmp = numbers;
+    Array<int> test(tmp);
     for (int i = 0; i < MAX_VAL; i++)
     {
         if (mirror[i] != numbers[i])
@@ -42,10 +42,33 @@ int main(int, char**)
     {
         std::cerr << e.what() << '\n';
     }
-
+    std::cout << "\nNumber value reset" << std::endl;
     for (int i = 0; i < MAX_VAL; i++)
     {
         numbers[i] = rand();
+    }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+        }
+    }
+    std::cout << "\ntmp and number" << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (tmp[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+        }
+    }
+    std::cout << "\ntmp and test" << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (tmp[i] != test[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+        }
     }
     delete [] mirror;
 	// system("leaks Array");
