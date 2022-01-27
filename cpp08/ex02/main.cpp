7cb4b0b77b232433a1a6769776abb9dc8d79fb35
stopminbeom <mseo@student.42seoul.kr>
2022-01-27 18:30:51 +0900

    Feat: cpp08 end2

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: mseo <mseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 16:29:07 by mseo              #+#    #+#             */
-/*   Updated: 2022/01/27 18:07:18 by mseo             ###   ########.fr       */
+/*   Updated: 2022/01/27 18:28:17 by mseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 #include "mutantstack.cpp"
+#include <list>
 
 int main()
 {
@@ -39,6 +40,33 @@ int main()
 		++it;
 	}
 	std::cout << std::endl;
+
+	std::cout << "List test" << std::endl;
+	std::list<int>	ltest;
+	ltest.push_back(5);
+	ltest.push_back(17);
+	std::cout << ltest.back() << std::endl;
+	ltest.pop_back();
+	std::cout << ltest.size() << std::endl;
+	ltest.push_back(3);
+	ltest.push_back(5);
+	ltest.push_back(737);
+	ltest.push_back(0);
+	std::list<int>::iterator lit = ltest.begin();
+	std::list<int>::iterator lite = ltest.end();
+	std::cout << "ltest begin : " << *lit << std::endl;
+	std::cout << "ltest end : "<< ltest.back() << std::endl;
+	++lit;
+	std::cout << "ltest begin + 1 : " << *lit << std::endl;
+	--lit;
+	std::cout << "ltest list : ";
+	while (lit != lite)
+	{
+		std::cout << *lit << " ";
+		++lit;
+	}
+	std::cout << std::endl;
+	
 	std::stack<int> s(mstack);
 	mstack.pop();
 	mstack.push(42);
